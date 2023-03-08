bc0897e5b015a58fc88ebdca9a1fb64912f3ac30
Пеньтюк Павел <pentukpavel@yandex.ru>
2023-03-08 17:53:28 +0300

    Homework telegram bot. Version 1

--- exceptions.py
+++ exceptions.py
@@ -1,8 +1,12 @@
 class TelegramBotExceptions(Exception):
+    """Базовый класс Исключений для телеграм бота."""
+
     pass
 
 
 class GeneralException(TelegramBotExceptions):
+    """Кастоиное исключение для работы телеграм бота."""
+
     def __init__(self, message):
         self.message = message
         super().__init__(self.message)
